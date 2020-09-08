2ff3757e943e1001d1aa97729be2de1200814056
pavelPonchatov <47302976+pavelPonchatov@users.noreply.github.com>
2020-09-09 00:05:37 +0400

    feat: add min table

--- src/views/App.js
+++ src/views/App.js
@@ -1,24 +1,16 @@
-import { Button, Dropdown, Menu } from "antd";
+import { Button, Dropdown, Menu, Table } from "antd";
 import React from "react";
 import "../assets/css/App.css";
 
+import Header from './header/Header'
+import TableInfo from './table/Table'
+
 function App() {
-  const menu = (
-    <Menu>
-      <Menu.Item>
-        <Button>one</Button>
-        <Button>two</Button>
-        <Button>three</Button>
-        <Button>four</Button>
-      </Menu.Item>
-    </Menu>
-  );
+
   return (
-    <div className="App">
-      <Button type="primary">Button</Button>
-      <Dropdown overlay={menu}>
-        <p>Hover me</p>
-      </Dropdown>
+    <div className='container'>
+      <Header/>
+      <TableInfo/>
     </div>
   );
 }
