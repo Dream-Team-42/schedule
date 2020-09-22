e6be024254119db254c843cf28eee28e5fc1197a
Aleksey Palanevich <palanevich.aleksey@gmail.com>
2020-09-22 22:13:05 +0300

    feat: switch student/mentor

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import "./assets/css/App.css";
 import Header from "./features/header/Header";
+import { selectUser } from "./features/header/headerSlice";
 import Lists from "./features/lists/Lists";
 import TaskForm from "./features/tasks/taskModal";
 
 function App() {
+  const user = useSelector(selectUser);
   return (
     <div className="container">
       <Header />
-      <TaskForm />
+      {user === "mentor" ? <TaskForm /> : ""}
       {/*<TableInfo/>*/}
       <Lists />
     </div>
